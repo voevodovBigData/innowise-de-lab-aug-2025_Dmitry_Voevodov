397541f1e36e2c1d2463a82680b5c4cd37916599
voevodovDmitry <mersisay@yandex.ru>
2025-08-25 23:43:04 +0300

    add HW7

---
+++ HW7/task_1.py
@@ -0,0 +1,4 @@
+shopping_list = ["milk", "bread", "eggs", "butter", "apples"]
+print("Purchase list:")
+for i, item in enumerate(shopping_list, start=1):
+    print(f"{i}. {item}")
---
+++ HW7/task_5.py
@@ -0,0 +1,5 @@
+a, b = int(input('Enter height of rectangle: ')), int(input('Enter wight of rectangle: '))
+for i in range(a):
+    for j in range(b):
+        print('*', end='')
+    print()
